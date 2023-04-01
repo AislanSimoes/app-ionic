612700a3ec18004e81d810168f7d53126a1485fc
AislanSimoes <54917228+AislanSimoes@users.noreply.github.com>
2023-04-01 12:49:01 -0300

    Implementação da página interna
    Criação da rota entre a Home e Detalhes;
    Estilização da página Detalhes;
    Melhoria na estilização da página Home;
    Criação de slides nos cards da Home;
    Criação de métodos para adição e remoção do carrinho;

--- src/app/detalhe/detalhe.page.ts
+++ src/app/detalhe/detalhe.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalhe.page.scss'],
 })
 export class DetalhePage implements OnInit {
+  pathImg = '../../assets/img/';
+  public produto = { nome: ' ', preco: ' ', descricao: " ", imagens: [] };
 
-  constructor() { }
+  public carrinho:any = []
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.produto = params['produto']
+    })
+  }
 
   ngOnInit() {
+  }
+
+  addProduto(produto: any) {
+    this.carrinho.push(produto);
+  }
+
+  removeProduto() {
+    this.carrinho.pop();
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,24 +15,24 @@ export class HomePage {
   public secao = 1;
 
   hamburguers = [
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"}
+    {nome: 'Hamburguer', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["hamburguer.png"]},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["hamburguer.png"]},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["hamburguer.png"]},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["hamburguer.png"]}
   ];
 
   pizzas = [
-    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
-    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
-    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
-    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"}
+    {nome: 'Pizza', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["pizza.jpg"]},
+    {nome: 'Pizza', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["pizza.jpg"]},
+    {nome: 'Pizza', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["pizza.jpg"]},
+    {nome: 'Pizza', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["pizza.jpg"]}
   ];
 
   cachorroes = [
-    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
-    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
-    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
-    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"}
+    {nome: 'Cachorrão', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["cachorrao.jpg"]},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["cachorrao.jpg"]},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["cachorrao.jpg"]},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis ligula non mattis aliquet. Nulla sed velit in lectus laoreet fringilla. Integer convallis, nunc id iaculis ultrices, ligula eros varius dui, eu finibus erat ex eget mi. Duis eu arcu ultrices dolor fermentum fringilla non sed magna. Mauris ipsum velit, maximus eget enim ac, euismod ullamcorper justo", imagens: ["cachorrao.jpg"]}
   ];
 
   categorias = [
@@ -40,10 +41,16 @@ export class HomePage {
     {id : 3, nome: "Cachorrão", produtos: this.cachorroes},
   ]
 
-  constructor() {
+  constructor(private navCtrl: NavController) {
   }
 
   public trocarSecao(categoria: any) {
     this.secao = categoria.id;
+  }
+
+  public verProduto(produto: any) {
+    this.navCtrl.navigateForward('detalhe', {
+      queryParams: { produto: produto }
+    });
   }
 }
