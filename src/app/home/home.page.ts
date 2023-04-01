cce845d3621171a47078eb2eaf1a3e1b2f880598
AislanSimoes <54917228+AislanSimoes@users.noreply.github.com>
2023-04-01 12:08:36 -0300

    Implementação e Estilização da Página Home
    Criação do layout;
    Estilização geral da página Home;
    Criação da lista de produtos e categorias;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,14 +7,43 @@ import { Component } from '@angular/core'
 })
 export class HomePage {
   pathImg = '../../assets/img/';
+  slideOpts = {
+    slidesPerView: 2
+  }
+
+  public secao = 1;
 
   hamburguers = [
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: ""},
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: ""},
-    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: ""},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"},
+    {nome: 'Hamburguer', preco: 'R$ 10,00', imagem: "hamburguer.png"}
   ];
+
+  pizzas = [
+    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
+    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
+    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"},
+    {nome: 'Pizza', preco: 'R$ 10,00', imagem: "pizza.jpg"}
+  ];
+
+  cachorroes = [
+    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"},
+    {nome: 'Cachorrão', preco: 'R$ 10,00', imagem: "cachorrao.jpg"}
+  ];
+
+  categorias = [
+    {id : 1, nome: "Pizza", produtos: this.pizzas },
+    {id : 2, nome: "Hamburger", produtos: this.hamburguers },
+    {id : 3, nome: "Cachorrão", produtos: this.cachorroes},
+  ]
 
   constructor() {
   }
 
+  public trocarSecao(categoria: any) {
+    this.secao = categoria.id;
+  }
 }
